93adfb3fe87aa0b73594b5173a25fd7fd4dc3d55
Mochammad Alif Kurniawan <mochammadalifkurniawan@gmail.com>
2022-11-18 13:02:50 +0700

    update

--- A_basics/B_variables_constant_types/C_types_conversion.py
+++ A_basics/B_variables_constant_types/C_types_conversion.py
@@ -4,7 +4,9 @@ float_num = 3.14
 print(f'float num type: {type(float_num)}')
 new_num = int_num + float_num # implicit_conversion -> automatically 
 print(f'new num type  : {type(new_num)}')
+
 print('---')
+
 str_num = "88"
 int2_num = 17
 str_num = int(str_num) # explicit_conversion -> v = T(v)
